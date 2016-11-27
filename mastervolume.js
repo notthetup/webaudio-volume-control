ef312eb263185c8782f9d3abb0f1d80dee15daa3
Chinmay Pendharkar <notthetup@gmail.com>
2016-11-27 18:11:42 +0800

    packaged for chrome

--- mastervolume.js
+++ mastervolume.js
@@ -1,48 +1,48 @@
 ///var a = new AudioContext(); osc = a.createOscillator(); osc.connect(a.destination); osc.start();
 
 if (typeof origAudioContext == 'undefined'){
-	// console.log("Hijacking...");
-	(function (){
-		var allAudioContexts = [];
-		window.origAudioContext = window.AudioContext;
-		window.AudioContext = function (){
-			var newAudioContextInstance = {};
-			var origACInstance = new origAudioContext();
-			var masterGain = origACInstance.createGain();
-			masterGain.connect(origACInstance.destination);
+  // console.log("Hijacking...");
+  (function (){
+    var allAudioContexts = [];
+    window.origAudioContext = window.AudioContext;
+    window.AudioContext = function (){
+      var newAudioContextInstance = {};
+      var origACInstance = new window.origAudioContext();
+      var masterGain = origACInstance.createGain();
+      masterGain.connect(origACInstance.destination);
 
-			for (var property in origACInstance) {
-				(function(){
-					var thisProp = property;
-					if (typeof(origACInstance[property]) === 'function'){
-						newAudioContextInstance[thisProp] = function () {
-							return origACInstance[thisProp].apply(origACInstance, arguments);
-						}
-					}else if(property !== "destination"){
-						Object.defineProperty(newAudioContextInstance, property, {
-							get : function (){
-								return origACInstance[thisProp]
-							}
-						});
-					}
-				})();
-			}
+      for (var property in origACInstance) {
+        (function(){
+          var thisProp = property;
+          if (typeof(origACInstance[property]) === 'function'){
+            newAudioContextInstance[thisProp] = function () {
+              return origACInstance[thisProp].apply(origACInstance, arguments);
+            }
+          }else if(property !== 'destination'){
+            Object.defineProperty(newAudioContextInstance, property, {
+              get : function (){
+                return origACInstance[thisProp]
+              }
+            });
+          }
+        })();
+      }
 
-			newAudioContextInstance.destination = masterGain;
-			newAudioContextInstance.gain = masterGain.gain;
+      newAudioContextInstance.destination = masterGain;
+      newAudioContextInstance.gain = masterGain.gain;
 
-			allAudioContexts.push(newAudioContextInstance);
-			// console.log("Running Hijacked Context", allAudioContexts);
+      allAudioContexts.push(newAudioContextInstance);
+      // console.log("Running Hijacked Context", allAudioContexts);
 
-			return newAudioContextInstance;
-		}
-		window.webkitAudioContext = window.AudioContext;
-		document.addEventListener("mastervolume", function(event){
-			// console.log("Adjusting master gain", event.detail, " of ", allAudioContexts.length);
-			allAudioContexts.forEach(function(thisContext){
-				thisContext.gain.value = parseFloat(event.detail.gainValue);
-			})
-		});
-	})()
-	// console.log("Hijacked");
+      return newAudioContextInstance;
+    }
+    window.webkitAudioContext = window.AudioContext;
+    document.addEventListener('mastervolume', function(event){
+      // console.log("Adjusting master gain", event.detail, " of ", allAudioContexts.length);
+      allAudioContexts.forEach(function(thisContext){
+        thisContext.gain.value = parseFloat(event.detail.gainValue);
+      })
+    });
+  })()
+  // console.log("Hijacked");
 }
--- volumecontrol.js
+++ volumecontrol.js
@@ -1,49 +1,47 @@
-if (typeof chrome === "undefined" && (typeof self === "undefined" || typeof self.port === "undefined")){
-	// console.log("neither in chrome nor FF");
+if (typeof chrome === 'undefined' && (typeof self === 'undefined' || typeof self.port === 'undefined')){
+  // console.log('neither in chrome nor FF');
 }else{
-		window.onload = function (){
-		console.log("controlling volume");
-		var storage;
-		var inputslider = document.getElementById('inputSlider');
-		var sliderlabel = document.getElementById('sliderLabel');
+  window.onload = function (){
+    // console.log('controlling volume');
+    var storage;
+    var inputslider = document.getElementById('inputSlider');
+    var sliderlabel = document.getElementById('sliderLabel');
 
-		if (typeof chrome !== "undefined" && chrome.storage){
-			storage = chrome.storage.local;
-			storage.get("mastervolume",function(result){
-				if (result && result.mastervolume){
-					console.log("old value found", result);
-					inputslider.value = result.mastervolume;
-					sliderlabel.innerHTML = result.mastervolume;
-					updateVolume(result.mastervolume/100.0);
-				}else{
-					console.log("creating new storage", parseFloat(inputslider.value));
-					storage.set({"mastervolume":parseFloat(inputslider.value)});
-				}
-			});
-		}
+    if (typeof chrome !== 'undefined' && chrome.storage){
+      storage = chrome.storage.local;
+      storage.get('mastervolume',function(result){
+        if (result && result.mastervolume){
+          // console.log('old value found', result);
+          inputslider.value = result.mastervolume;
+          sliderlabel.innerHTML = result.mastervolume;
+          updateVolume(result.mastervolume/100.0);
+        }else{
+          // console.log('creating new storage', parseFloat(inputslider.value));
+          storage.set({'mastervolume':parseFloat(inputslider.value)});
+        }
+      });
+    }
 
-		inputslider.addEventListener('input', function(){
-			sliderlabel.innerHTML = parseInt(inputslider.value);
-			var volume = parseFloat(inputslider.value)/100.0;
+    inputslider.addEventListener('input', function(){
+      sliderlabel.innerHTML = parseInt(inputslider.value);
+      var volume = parseFloat(inputslider.value)/100.0;
 
-			if(typeof chrome !== "undefined"){
-				storage.set({"mastervolume":parseFloat(inputslider.value)});
-			}
+      if(typeof chrome !== 'undefined'){
+        storage.set({'mastervolume':parseFloat(inputslider.value)});
+      }
 
-			updateVolume(volume);
-		})
-	}
+      updateVolume(volume);
+    })
+  }
 }
 
 function updateVolume(volume){
-	if (typeof chrome !== "undefined"){
-		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		  chrome.tabs.sendMessage(tabs[0].id, {mastervolume: volume});
-		});
-	}else if (typeof self !== "undefined" && self.port){
-		// console.log("emiting..", volume);
-		self.port.emit("mastervolume",volume);
-	}
+  if (typeof chrome !== 'undefined'){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {mastervolume: volume});
+    });
+  }else if (typeof self !== 'undefined' && self.port){
+    // console.log('emiting..', volume);
+    self.port.emit('mastervolume',volume);
+  }
 }
-
-
